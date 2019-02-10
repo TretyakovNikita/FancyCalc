da427ac74314bdff7c008100f1b52da3788fe976
Nikita Tret <accauntnikity@gmail.com>
2019-02-10 22:47:22 +0300

    Update FancyCalc.cs

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FancyCalc
 {
     public class FancyCalcEnguine
     {
+        private const char plus = '+';
+        private const char minus = '-';
+        private const char mult = '*';
 
         public double Add(int a, int b)
         {
@@ -22,15 +22,38 @@ namespace FancyCalc
 
 
         public double Multiply(int a, int b)
-        {   
+        {
             return a * b;
         }
 
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
+            if (expression is null)
+            {
+                throw new ArgumentNullException();
+            }
 
+            expression = expression.Replace(" ", string.Empty);
+            var parameters = expression.Split(plus, minus, mult);
+            char sign = expression[parameters[0].Length];
+
+            int firstNumber = int.Parse(parameters[0]);
+            int secondNumber = int.Parse(parameters[1]);
+            switch (sign)
+            {
+                case plus:
+                    return Add(firstNumber, secondNumber);
+                case minus:
+                    return Subtract(firstNumber, secondNumber);
+                case mult:
+                    return Multiply(firstNumber, secondNumber);
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
+
+        
+
